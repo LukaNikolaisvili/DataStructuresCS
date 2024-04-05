1fe32a524b5dfb8408050727e9541d8b0774dbb7
Luka Nikolaisvili <96894576+LukaNikolaisvili@users.noreply.github.com>
2024-04-04 20:38:47 -0400

    Update assn3.cs
    fixed the structure of the code, organized it by classess[now all the methods are in their class] and finished couple methods for the lazy binomial heap, as well as tested the bstforBtree and fixed print methods for lazyB

--- assn3.cs
+++ assn3.cs
@@ -1,260 +1,297 @@
-﻿/*
-
-COIS-3020 ASSIGNMENT 3
-CONTRIBUTORS:
-LUKA NIKOLAISVILI
-FARZAD IMRAN 
-FREDERICK NKWONTA
-DUE DATE: APR 7th 2024
-
-*/
-
-
 using System;
+using System.Collections.Generic;
 
+// Part A: Point Quadtree
 public class PointQuadTree
 {
     private Node[] B;
 
     public PointQuadTree(int dimensions)
     {
-        // Initialize B array
         B = new Node[(int)Math.Pow(2, dimensions)];
     }
 
-    // ComparePoints method compares two d-dimensional points and maps the result to an index
     public int ComparePoints(int[] point1, int[] point2)
     {
         int index = 0;
         for (int i = 0; i < point1.Length; i++)
         {
-            // Update index based on the comparison of coordinates at each dimension
-            if (point1[i] < point2[i])
-                index = 2 * index + 1;  // Left branch
-            else
-                index = 2 * index + 2; // Right branch
+            index = point1[i] < point2[i] ? 2 * index + 1 : 2 * index + 2;
         }
         return index;
-
     }
-
-
 
     private class Node
     {
-        // Define Node structure
-
+        // Node structure implementation
     }
-
-
-    public static void Main(string[] args)
-    {
-
-
-
-        // Test the comparison of points
-        PointQuadTree quadTree = new PointQuadTree(2); // 2-dimensional space
-        int[] point1 = { 3, 4 };
-        int[] point2 = { 2, 5 };
-
-        int index = quadTree.ComparePoints(point1, point2);
-        Console.WriteLine("Index: " + index);
-        //         int count = 1;
-        //         while (count <= 3)
-        //         {
-        //             TwoThreeFourTree<int> tree = new TwoThreeFourTree<int>();
-        //             tree.Insert(1);
-        //             tree.Insert(2);
-        //             tree.Insert(3);
-        //             tree.Print();
-        //             Console.WriteLine("Last Assignment Wooohooo!");
-        //             Console.WriteLine(".\n" +
-        // "   ……..…../´¯/)………… (\\¯`\\\n" +
-        // "   …………/….//……….. …\\\\….\\\n" +
-        // "   ………../….//………… ….\\\\….\\\n" +
-        // "   …../´¯/…./´¯\\………../¯ `\\….\\¯`\\\n" +
-        // "   .././…/…./…./.|_……_| .\\….\\….\\…\\\n" +
-        // "   (.(….(….(…./.)..)..(..(. \\….)….)\n" +
-        // "   .\\…………….\\/…/….\\. ..\\/……………./\n" +
-        // "   ..\\…………….. /……..\\……………..…/\n" +
-        // "    ….\\…………..(…………)……………./\n" +
-        // "   ");
-        //             count++;
-        //         }
-
-
-
-    }
-
-
 }
 
-public class LazyBinomialHeap<T>
+// Part B: Lazy Binomial Heap
+public class LazyBinomialHeap<T> where T : IComparable<T>
 {
+    private Dictionary<int, List<BinomialNode>> roots;
+    private BinomialNode minNode;
+
     private class BinomialNode
     {
-        // Define BinomialNode structure
+        public T Key { get; set; }
+        public List<BinomialNode> Children { get; set; }
+        public int Order { get; set; }
 
+        public BinomialNode(T key)
+        {
+            Key = key;
+            Children = new List<BinomialNode>();
+            Order = 0;
+        }
     }
-
-    private BinomialNode[] B;
 
     public LazyBinomialHeap()
     {
-        // Initialize Lazy Binomial Heap
+        roots = new Dictionary<int, List<BinomialNode>>();
+        minNode = null;
     }
 
     public void Add(T item)
     {
-        // Add method implementation
+        var newNode = new BinomialNode(item);
+        if (!roots.ContainsKey(0))
+        {
+            roots[0] = new List<BinomialNode>();
+        }
+        roots[0].Add(newNode);
+
+        if (minNode == null || item.CompareTo(minNode.Key) < 0)
+        {
+            minNode = newNode;
+        }
     }
 
     public T Front()
     {
-        // Front method implementation
-        return default(T);
+        if (minNode == null)
+        {
+            throw new InvalidOperationException("Heap is empty");
+        }
+        return minNode.Key;
     }
 
     public void Remove()
     {
-        // Remove method implementation
+        // Implement the Remove method
     }
 
     public void Print()
     {
-        // Print method implementation
+        foreach (var nodeList in roots.Values)
+        {
+            foreach (var node in nodeList)
+            {
+                PrintTree(node, 0);
+                Console.WriteLine(); // For better readability between trees
+            }
+        }
     }
 
-    private void Coalesce()
+    private void PrintTree(BinomialNode node, int depth)
     {
-        // Coalesce method implementation
+        Console.Write(new string(' ', depth * 2) + node.Key);
+        foreach (var child in node.Children)
+        {
+            Console.WriteLine();
+            PrintTree(child, depth + 1);
+        }
     }
-
-
 }
 
+// Part C: 2-3-4 Trees to Red Black Trees
 public class TwoThreeFourTree<T> where T : IComparable<T>
 {
-    private Node<T> root;
-    private class Node<T>
-    {
-        private int n; // number of keys
-        private bool leaf; // true if a leaf node; false otherwise
-        private T[] key; // array of keys
-        private Node<T>[] c; // array of child references
-        public Node(int t)
-        {
-            n = 0;
-            leaf = true;
-            key = new T[2 * t - 1];
-            c = new Node<T>[2 * t];
-        }
-        //Public getter method for the key array
-        public T[] getKey()
-        {
-            return key;
-        }
-
-        // Public method to check if the value is a leaf node
-        public bool checkLeaf()
-        {
-            return leaf;
-        }
-
-        // Public method to get child node references
-        public Node<T>[] getChildren()
-        {
-            return c;
-        }
-
-    }
-    // Initializes an empty 2-3-4 tree. (2 marks)
     public TwoThreeFourTree()
     {
-        root = null;
+        // Constructor implementation
     }
 
-    // Returns true if key k is successfully inserted; false otherwise. (6 marks)
     public bool Insert(T k)
     {
+        // Implement the Insert method
         return false;
     }
 
-    // Returns true if key k is successfully deleted; false otherwise. (10 marks)
     public bool Delete(T k)
     {
+        // Implement the Delete method
         return false;
     }
 
-    // Returns true if key k is found; false otherwise (4 marks).
     public bool Search(T k)
     {
-        Node<T> p = root;
-        T[] keys = p.getKey();
-        Node<T>[] children;
-        int length = 0;
-
-        while (p != null) // Recursion without a recursive call
-        {
-            foreach (T key in keys)
-            {
-                if (key.Equals(k))
-                {
-                    return true; // Key found
-                }
-            }
-
-            if (p.checkLeaf() == true)
-            {
-                return false;
-            }
-            else
-            {
-                // Here onwards is iffy and might be completely incorrect process wise, Instructions are as follows:
-
-                /*  If the key k is not found and p is a leaf node then return false
-                    else move to the subtree which would otherwise contain k    */
-                // I do not fully know the logic to find the right subtree and dont want to use a for loop for each
-
-                children = p.getChildren();
-                length = children.Length;
-
-                p = children[0]; // bogus line for now, change with proper index and method to find index
-
-                if () // some statement to find the right subtree index
-                {
-
-                }
-                else // no correct subtree index. Exit
-                {
-                    return false;
-                }
-            }
-        }
-
-        // Key not found despite all of this
-        // Return false
+        // Implement the Search method
         return false;
     }
 
-    // Builds and returns the equivalent red-black tree. (8 marks).
-    public BSTforRBTree<T> Convert()
-    {
-        // To do, use the BSTforRBTree file provided (Patrick said so)
-    }
+    //we have to check this!!!
 
-    // Prints out the keys of the 2-3-4 tree in order. (4 marks)
+    // public BSTforRBTree<int> Convert()
+    // {
+    //     // Implement the Convert method
+    //     return new BSTforRBTree<int>();
+    // }
+
     public void Print()
     {
-
+        // Implement the Print method
     }
-
 }
 
+public enum Color { RED, BLACK }; // Colors of the red-black tree
+
+public interface ISearchable<T>
+{
+    void Add(T item, Color rb);
+    void Print();
+}
+
+//-------------------------------------------------------------------------
+
+// Implementation:  BSTforRBTree
+public class BSTforRBTree<T> : ISearchable<T> where T : IComparable
+{
+    // Common generic node class for a BSTforRBTree
+    private class Node
+    {
+
+        // Read/write properties
+        public T Item;
+        public Color RB;
+        public Node Left;
+        public Node Right;
+
+        public Node(T item, Color rb)
+        {
+            Item = item;
+            RB = rb;
+            Left = Right = null;
+        }
+    }
+
+    private Node root;
+
+    public BSTforRBTree()
+    {
+        root = null; // Empty BSTforRBTree
+    }
+
+    // Add 
+    // Insert an item into a BSTforRBTRee
+    // Duplicate items are not inserted
+    // Worst case time complexity:  O(log n) 
+    // since the maximum depth of a red-black tree is O(log n)
+
+    public void Add(T item, Color rb)
+    {
+        Node curr;
+        bool inserted = false;
+
+        if (root == null)
+            root = new Node(item, rb);   // Create a root
+        else
+        {
+            curr = root;
+            while (!inserted)
+            {
+                if (item.CompareTo(curr.Item) < 0)
+                {
+                    if (curr.Left == null)              // Empty spot
+                    {
+                        curr.Left = new Node(item, rb);
+                        inserted = true;
+                    }
+                    else
+                        curr = curr.Left;               // Move left
+                }
+                else
+                    if (item.CompareTo(curr.Item) > 0)
+                {
+                    if (curr.Right == null)         // Empty spot
+                    {
+                        curr.Right = new Node(item, rb);
+                        inserted = true;
+                    }
+                    else
+                        curr = curr.Right;          // Move right
+                }
+                else
+                    inserted = true;                // Already inserted
+            }
+        }
+    }
+
+    public void Print()
+    {
+        Print(root, 0);                // Call private, recursive Print
+        Console.WriteLine();
+    }
+
+    // Print
+    // Inorder traversal of the BSTforRBTree
+    // Time complexity:  O(n)
+
+    private void Print(Node node, int k)
+    {
+        string s;
+        string t = new string(' ', k);
+
+        if (node != null)
+        {
+            Print(node.Right, k + 4);
+            s = node.RB == Color.RED ? "R" : "B";
+            Console.WriteLine(t + node.Item.ToString() + s);
+            Print(node.Left, k + 4);
+        }
+    }
+}
+
+// Main class for testing
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Quadtree
+        PointQuadTree quadTree = new PointQuadTree(2); // 2-dimensional space
+        int[] point1 = { 3, 4 };
+        int[] point2 = { 2, 5 };
+        int index = quadTree.ComparePoints(point1, point2);
+        Console.WriteLine("\nIndex: " + index);
+
+        Console.WriteLine("\nLazyBinomialHeap TESTING");
+        Console.WriteLine("\n-------------------------\n");
+        // Testing LazyBinomialHeap
+        LazyBinomialHeap<int> heap = new LazyBinomialHeap<int>();
+        heap.Add(1);
+        heap.Add(2);
+        heap.Add(3);
+        heap.Add(4);
+        heap.Add(5);
+        heap.Print();
+        Console.WriteLine("\n-------------------------\n");
+        Console.WriteLine("\nBST TESTING");
+        Console.WriteLine("\n-------------------------\n");
+        BSTforRBTree<int> bst = new BSTforRBTree<int>();
+        Color red = new Color();
+        red = Color.RED;
+        Color black = new Color();
+        black = Color.BLACK;
+        
+        bst.Add(10,red);
+        bst.Add(20,black);
+        bst.Add(12,red);
+        bst.Add(25,black);
+
+        bst.Print();
+        Console.WriteLine("\n-------------------------");
 
 
-
-
-
-
-
+    }
+}
